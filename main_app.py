7eae0ad16d16b57825c9c03d2522e4204586531f
Michael Stattelman <mstatt@users.noreply.github.com>
2021-10-11 20:46:45 +0400

    Revert

--- main_app.py
+++ main_app.py
@@ -1,6 +1,7 @@
-
-#env FLASK_APP=main_app.py python -m flask run
+#To run the application:
+#    streamlit run main_app.py
 #Library imports
+import tensorflow as tf
 import numpy as np
 import streamlit as st
 import cv2
@@ -8,38 +9,39 @@ from keras.models import load_model
 
 
 #Loading the Model
-model = load_model('plant_disease.h5')
+model = load_model('falcon_types.h5')
 
 #Name of Classes
-CLASS_NAMES = ['Corn-Common_rust', 'Potato-Early_blight', 'Tomato-Bacterial_spot']
+CLASS_NAMES = ['Gyr', 'Peregrine', 'Saker']
 
 #Setting Title of App
-st.title("Plant Disease Detection")
-st.markdown("Upload an image of the plant leaf")
+st.title("Falcon Type Prediction")
+st.markdown("Upload an image of the Falcon")
 
 #Uploading the dog image
-plant_image = st.file_uploader("Choose an image...", type="jpg")
+falcon_image = st.file_uploader("Choose an image...", type="jpg")
 submit = st.button('Predict')
 #On predict button click
 if submit:
 
 
-    if plant_image is not None:
-
+    if falcon_image is not None:
         # Convert the file to an opencv image.
-        file_bytes = np.asarray(bytearray(plant_image.read()), dtype=np.uint8)
+        file_bytes = np.asarray(bytearray(falcon_image.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
+        
+        #Resizing the image
+        opencv_image = cv2.resize(opencv_image, (280,280))
+        #Convert image to 4 Dimension
+        opencv_image.shape = (1,280,280,3)
+        #Make Prediction
+        predictions = model.predict(opencv_image)
+        score = tf.nn.softmax(predictions[0])
 
 
+        st.title(str("This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(CLASS_NAMES[np.argmax(score)], 100 * np.max(score))))
 
         # Displaying the image
         st.image(opencv_image, channels="BGR")
-        st.write(opencv_image.shape)
-        #Resizing the image
-        opencv_image = cv2.resize(opencv_image, (256,256))
-        #Convert image to 4 Dimension
-        opencv_image.shape = (1,256,256,3)
-        #Make Prediction
-        Y_pred = model.predict(opencv_image)
-        result = CLASS_NAMES[np.argmax(Y_pred)]
-        st.title(str("This is "+result.split('-')[0]+ " leaf with " + result.split('-')[1]))
+        #st.write(opencv_image.shape)
