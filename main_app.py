87df74a55146b2d303425a5121e3d51c25636ffd
Michael Stattelman <mstatt@users.noreply.github.com>
2021-10-13 08:40:04 +0400

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -17,6 +17,13 @@ CLASS_NAMES = ['Gyr-Saker',  'Peregrine', 'Saker']
 #Setting Title of App
 st.title("Falcon Type Prediction")
 st.markdown("Upload an image of the Falcon")
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 #Uploading the dog image
 falcon_image = st.file_uploader("Choose an image...", type="jpg")
@@ -39,9 +46,8 @@ if submit:
         score = tf.nn.softmax(predictions[0])
 
 
-        st.title(str("This image most likely belongs to {} with a {:.2f} percent confidence."
+        st.title(str("This image is most likely a {} falcon."
     .format(CLASS_NAMES[np.argmax(score)], 100 * np.max(score))))
 
         # Displaying the image
         st.image(opencv_image, channels="BGR")
-        #st.write(opencv_image.shape)
